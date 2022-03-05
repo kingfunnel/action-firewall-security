3787487474a71921f482ada713c10641cd6ceb45
anpimartinez <kingfunnel.co@gmail.com>
2022-03-05 16:18:04 -0500

    Action Firewall Security 1.2.8

---
+++ action-firewall.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+Plugin Name: Action Firewall Security
+Plugin URI: https://actionfirewallsecurity.org
+Description: A true Web Application Firewall to protect and secure WordPress.
+Version: 1.2.8
+Author: The Ninja Technologies Network
+Author URI: https://actionfirewallsecurity.org
+License: GPL 2+
+License URI: https://actionfirewallsecurity.org
+*/ 
+
+
+?>
